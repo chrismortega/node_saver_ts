f0a7ebfbf2293760c6f6352f208c90b0648f4bd3
Christopher Ortega <chris.ortega@e-farmcredit.com>
2020-02-22 11:51:04 -0500

    add webpackhotmodule

--- _src/index.ts
+++ _src/index.ts
@@ -24,7 +24,24 @@ const server = app.listen(PORT, () => {
 });
 
 // Webpack Hot-Module Replacement Activation
+type ModuleId = string | number;
+
+interface WebpackHotModule {
+  hot?: {
+    data: any;
+    accept(
+      dependencies: string[],
+      callback?: (updatedDependencies: ModuleId[]) => void,
+    ): void;
+    accept(dependency: string, callback?: () => void): void;
+    accept(errHandler?: (err: Error) => void): void;
+    dispose(callback: (data: any) => void): void;
+  };
+}
+
+declare const module: WebpackHotModule;
+
 if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => server.close());
+  module.hot.accept();
+  module.hot.dispose(() => server.close());
 }
