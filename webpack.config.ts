7ff8eeb4fe6f8fc0e6fe0c84a66d61ff3c796f5d
Christopher Ortega <cortega.code@gmail.com>
2020-02-23 14:47:24 -0500

    add docker support. had to set webpack to not watch for now

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,7 +5,7 @@ const nodeExternals = require("webpack-node-externals");
 module.exports = {
   entry: ["webpack/hot/poll?100", "./_src/index.ts"],
   devtool: 'source-map',
-  watch: true,
+  watch: false,
   target: "node",
   externals: [
     nodeExternals({
